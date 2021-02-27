67914579866498e8b8c4d9f1c385bdacba4ee5a8
Wong Zhi Kai <wzhikai@gmail.com>
2021-02-27 14:23:42 +0800

    cloudinary added to model

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :doses, :ingredients)
+    params.require(:cocktail).permit(:name, :doses, :ingredients, :photo)
   end
 end
