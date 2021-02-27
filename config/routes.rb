68fb10cd4024cd899d1481e6abfa39e4bce1d436
Wong Zhi Kai <wzhikai@gmail.com>
2021-02-27 13:44:00 +0800

    initial heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  get "cocktails", to: "cocktails#index"
+  get "cocktails/new", to: "cocktails#new"
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
+
+  post "cocktails", to: "cocktails#create"
+
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url_read = open(url).read
+ingredients = JSON.parse(url_read)
+ingredients['drinks'].each do |ing| Ingredient.create(name: ing["strIngredient1"]) end
+
+Cocktail.create(name: 'Gin and tonic')
